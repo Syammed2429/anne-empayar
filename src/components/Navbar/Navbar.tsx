559f5246c38fa2ae31e776620321c16550432553
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-27 13:41:42 +0530

    added a color toggle button

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { SimpleGrid, useColorMode, Button } from "@chakra-ui/react";
+import { BiMoon, BiSun } from "react-icons/bi";
+import { useColorMode, Flex, Button, Box } from "@chakra-ui/react";
 
 const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <>
-      <header>
-        <Button onClick={toggleColorMode}>
-          Toggle {colorMode === "light" ? "Dark" : "Light"}
-        </Button>
-      </header>
+      <Box boxShadow="dark-lg" p={1}>
+        <Flex justify="flex-end" m={2}>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <BiMoon /> : <BiSun />}
+          </Button>
+        </Flex>
+      </Box>
     </>
   );
 };
