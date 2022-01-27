dda9fef95b5898a389f4f6b426b5d637c908f949
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-27 14:24:48 +0530

    added fb and yt logos

--- src/components/Connect/Connect.tsx
+++ src/components/Connect/Connect.tsx
@@ -1,10 +1,22 @@
 import React, { FC } from "react";
-import { Center } from "@chakra-ui/react";
+import { Center, Text, Box, Flex, Container } from "@chakra-ui/react";
+import { FaFacebook } from "react-icons/fa";
+import { BsYoutube } from "react-icons/bs";
 
 const Connect: FC = () => {
   return (
     <>
-      <Center></Center>
+      <Container maxW="xl" centerContent my={2}>
+        <Box my={2}>
+          <Text>Connect With Me</Text>
+        </Box>
+        <Box>
+          <Flex gap={15}>
+            <FaFacebook color="blue" fontSize="45" />
+            <BsYoutube color="red" fontSize="45" />
+          </Flex>
+        </Box>
+      </Container>
     </>
   );
 };
