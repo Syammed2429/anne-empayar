c8f38152779ed9f1bf958305b4b104f2ed446ce9
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-27 13:44:32 +0530

    created layout component to import all local components at one place

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { About } from "../About/About";
+import { Navbar } from "../Navbar/Navbar";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <About />
+    </>
+  );
+};
+
+export { Layout };
