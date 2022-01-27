b999a0d1d8ce7e490a7407fd1cea93f563cb7cde
Dada Khalandar <kerry.blig12@gmail.com>
2022-01-27 13:43:21 +0530

    building about component

---
+++ src/components/About/About.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container, Text } from "@chakra-ui/react";
+const About = () => {
+  return (
+    <>
+      <Container>
+        <Text>Hai</Text>
+      </Container>
+    </>
+  );
+};
+
+export { About };
